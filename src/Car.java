652643706fb451ee34f6399088ec5d1f2562ee82
takut <takut@10.80.156.128>
2025-11-13 16:52:46 +0900

    2

--- src/Car.java
+++ src/Car.java
@@ -1,22 +1,44 @@
 
 public class Car {
-	int x,y,vx,vy;
-	public Car(int x, int y, int vx, int vy) {
+	int x,y,w,h,vx,vy;
+	public Car(int x, int y, int w, int h, int vx, int vy) {
 		this.x=x;
 		this.y=y;
+		this.w=w;
+		this.h=h;
 		this.vx=vx;
 		this.vy=vy;
+		
 	}
 	
 	public void draw(MyFrame2 frame) {
-		frame.fillOval(x, y, 50, 100);
-		frame.fillOval(x+100, y, 50, 100);
-		frame.fillRect(x, y+160, 150, 10);
+
+        int ux = w/8;
+        int uy = h/6;
+
+        frame.fillRect(x-ux*2, y-uy*3, ux*4, uy*2);
+        frame.fillRect(x-ux*4, y-uy, ux*8, uy*2);
+        frame.fillOval(x-ux*2-uy, y+uy, uy*2, uy*2);
+        frame.fillOval(x+ux*2-uy, y+uy, uy*2, uy*2);
 	}
+
 	
 	public void move() {
 		x+=vx;
 		y+=vy;
+	}
+	
+	public void stop() {
+		this.vx=0;
+		this.vy=0;
+	}
+	
+	public boolean isMoving() {
+		if(vx!=0 || vy!=0) {
+			return true;
+		}else {
+			return false;
+		}
 	}
 
 }
--- src/DisplayCarObject.java
+++ src/DisplayCarObject.java
@@ -1,15 +1,27 @@
 
 public class DisplayCarObject extends MyFrame2{
+	int n=2;
 	public void run() {
-		Car car1=new Car(50,50,10,5);
-		Car car2=new Car(200,100,-10,-5);
+		
+		Car car0=new Car(50+10*n,350+10*n,32, 24,n+5, -n-5);
+		Car car1=new Car(450-10*n,250-10*n,32, 24,-n-5, n+5);
+		
 		
 		for(int i=0; i<300; i++) {
 			clear();
+			drawLine(300, 0, 300, 500);
+			car0.draw(this);
 			car1.draw(this);
-			car2.draw(this);
+			car0.move();
 			car1.move();
-			car2.move();
+			
+			if(car0.x>=300) {
+				car0.stop();
+			}
+			
+			if(car1.x<=300) {
+				car1.stop();
+			}
 			
 			sleep(0.1);
 			
@@ -18,7 +30,6 @@ public class DisplayCarObject extends MyFrame2{
 	
 	public static void main(String[] args) {
 		MyFrame2 f=new  DisplayCarObject();
+		f.setSize(600,500);
 	}
-
-
 }
